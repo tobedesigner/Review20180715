d39628b95d212089e59dd848f2626dd7a3b7e75b
tobedesigner <tobedesigner@gmail.com>
2018-07-17 23:24:46 +0800

    30 隨堂測驗：練習透過 Razor 語法精準輸出指定格式

---
+++ MVC5Course/Controllers/RazorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class RazorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+
+        public ActionResult RazorView()
+        {
+            //30 隨堂測驗：練習透過 Razor 語法精準輸出指定格式
+            return View();
+        }
+    }
+}
